183d47b011f6a10f58455ab7a1693221dff65c22
GShacklebolt <ghazali123456@gmail.com>
2020-02-21 12:03:16 +0530

    Connection host

--- common/connection.php
+++ common/connection.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost:3307","root","","quedicate");
-//$con=mysqli_connect("fdb25.awardspace.net","2979936_temp1","Temporary1","2979936_temp1");
+//$con=mysqli_connect("localhost:3307","root","","quedicate");
+$con=mysqli_connect("fdb25.awardspace.net","2979936_temp1","Temporary1","2979936_temp1");
 ?>
